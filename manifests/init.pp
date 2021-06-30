625fe9ca209ce9dbc6ba9e565ee2b7d4698207fa
Hannes Schaller <hannes.schaller@apa.at>
2021-06-30 11:41:56 +0200

    implement country parameter

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,20 +6,8 @@
 #
 # Document parameters here.
 #
-# [*sample_parameter*]
-#   Explanation of what this parameter affects and what it defaults to.
-#   e.g. "Specify one or more upstream ntp servers as an array."
-#
-# === Variables
-#
-# Here you should define a list of variables that this module would require.
-#
-# [*sample_variable*]
-#   Explanation of how this variable affects the funtion of this class and if
-#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
-#   External Node Classifier as a comma separated list of hostnames." (Note,
-#   global variables should be avoided in favor of class parameters as
-#   of Puppet 2.6.)
+# [*countries*]
+#   Array of countries that should be accepted to login
 #
 # === Examples
 #
@@ -31,9 +19,11 @@
 #
 # === Copyright
 #
-# Copyright 2015 Your name here, unless otherwise noted.
+# Copyright 2015 Hannes Schaller
 #
-class geoipcheck {
+class geoipcheck (
+  Array $countries = [ 'AT', 'UK' ],
+){
 
   package {['geoip-bin', 'geoip-database']:
     ensure => present
--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,7 +1,24 @@
 require 'spec_helper'
 describe 'geoipcheck' do
-
   context 'with defaults for all parameters' do
-    it { should contain_class('geoipcheck') }
+    it { is_expected.to contain_class('geoipcheck') }
+  end
+
+  context 'with country parameter' do
+    let(:params) do
+      { 'countries' => [ 'US', 'GE' ] }
+    end
+
+    it { is_expected.to contain_class('geoipcheck') }
+    it { is_expected.to contain_file('/usr/local/geoip/check').with_content(%r{ALLOW_COUNTRIES="US GE"}) }
+  end
+
+  context 'with one country' do
+    let(:params) do
+      { 'countries' => [ 'US' ] }
+    end
+
+    it { is_expected.to contain_class('geoipcheck') }
+    it { is_expected.to contain_file('/usr/local/geoip/check').with_content(%r{ALLOW_COUNTRIES="US"}) }
   end
 end
